7623726b9503a3225876ece1e542bc6c5b55b45d
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-17 17:59:15 +0300

    fix(route): reorder imports and remove unnecessary blank lines

--- src/routes/_app/route.tsx
+++ src/routes/_app/route.tsx
@@ -1,8 +1,8 @@
 // oxlint-disable react/function-component-definition func-style
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
-import { getUserSession } from "#/features/auth/functions/index.ts";
 import { Navbar } from "#/components/shared/navbar.tsx";
+import { getUserSession } from "#/features/auth/functions/index.ts";
 
 export const Route = createFileRoute("/_app")({
 	component: RouteComponent,
@@ -20,7 +20,7 @@ function RouteComponent() {
 	return (
 		<div className="flex min-h-screen flex-col">
 			<Navbar />
-			
+
 			<main className="flex-1">
 				<Outlet />
 			</main>
